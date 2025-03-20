355aa2c9210093e5f39cdd127654720cd527bbc9
rahul <rahul.p@9xtechnology.com>
2025-03-20 11:15:43 +0530

    Changed export

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const Employee = require("./models/Employee");
-const ProfilePic = require("./models/ProfilePic");
+module.exports = (mongoose) => {
+  const Employee =
+    mongoose.models.Employee ||
+    mongoose.model("Employee", require("./employeeSchema"));
+  const ProfilePic =
+    mongoose.models.ProfilePic ||
+    mongoose.model("ProfilePic", require("./profilePicSchema"));
 
-module.exports = { Employee, ProfilePic };
+  return { Employee, ProfilePic };
+};
